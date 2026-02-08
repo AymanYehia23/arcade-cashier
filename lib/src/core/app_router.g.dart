ba8af793efdd8359729e1de18d25cefa35775de1
Amrmahmoud333 <amr.mahmoud.elsayed33@gmail.com>
2026-02-08 22:08:15 +0200

    Add tables routes to app navigation

--- lib/src/core/app_router.g.dart
+++ lib/src/core/app_router.g.dart
@@ -6,7 +6,7 @@ part of 'app_router.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$goRouterHash() => r'8d1e6b304f81bebdc27dc751c6ba1a5111de074e';
+String _$goRouterHash() => r'ccb2b69b767f277381311a7dbe55445dbf8598bf';
 
 /// See also [goRouter].
 @ProviderFor(goRouter)
