a8445020f799afa9f2dc1769e0095bee3b15d9a3
AymanYehia23 <AymanYehia23@users.noreply.github.com>
2026-02-21 20:04:45 +0000

    deploy: 5ac925ce93439fd4a0103da9529ee7c9c2c5a741

--- android/app/src/main/kotlin/com/example/arcade_cashier/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.arcade_cashier
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/src/utils/double_extension.dart
+++
@@ -1,8 +0,0 @@
-import 'package:intl/intl.dart';
-
-extension DoubleExtension on double {
-  String toSmartString() {
-    final formatter = NumberFormat("#,##0.##", "en_US");
-    return formatter.format(this);
-  }
-}
